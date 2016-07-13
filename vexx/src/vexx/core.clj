6a74848f152cf740e2c6abfb306945746dc8645c
kowaraj <kowaraj4stuff@gmail.com>
2016-07-13 18:24:26 +0200

    up

--- vexx/src/vexx/core.clj
+++ vexx/src/vexx/core.clj
@@ -11,12 +11,21 @@
   (println "handle-key-typed: " c))
 
 
+
 (def list-data (ref [1 2 3]))
+;; (let [new-el "new text"
+;;       old-list-data @list-data
+;;       new-list-data (conj old-list-data new-el)]
+;;   (prn new-list-data)
+;;   )
+
+
 (defn list-model 
   [items]
   (let [model (javax.swing.DefaultListModel.)]
     (doseq [item items] (.addElement model item))
     model))
+
 
 
 (defn add-behavior [root]
@@ -25,6 +34,7 @@
         xl (ss/select p [:#xlist])
         xt (ss/select p [:#xtext])
         text-in (ss/select p [:#text-in])
+        text-log (ss/select p [:#text-log])
         ]
 
     (add-watch list-data nil
@@ -43,29 +53,47 @@
                             ))
     (ss/listen text-in
                :key-typed (fn [e]
-                            (handle-key-typed (.getKeyChar e))
-                            (dosync (ref-set list-data [4 5 6 7 0]))
-                            ))
+                            (let [ch (.getKeyChar e)]
+                              (if (= ch \newline)
+                                (let [new-el (ss/text text-in)
+                                      old-list-data @list-data
+                                      new-list-data (conj old-list-data new-el)]
+                                  (ss/config! text-log :text (str new-list-data))
+                                  (dosync (ref-set list-data new-list-data)))
+                                (ss/config! text-log :text (str "symbol you typed: " (.getKeyChar e)))
+                                ))))
     ;;;(.requestFocusInWindow p)
     root))
-
+;(-main)
+(def v [1 2 3])
+(conj v 5)
+(str v)
 
 (defn make-content []
-  (ssm/mig-panel
-   :id :panel
-   :items[
-          ["Propeller"        "split, span, gaptop 10"]
-          [(ss/toggle :id :pencil  :class :tool :text "Pencil" )]
-          [(ss/toggle :id :line    :class :tool :text "Line")]
-          [(ss/combobox :id :stroke :class :style :model [1 2 3 5 8 13 21])"wrap"]
-          [(ss/text :id :text-in
-                    :text "input" :editable? true :columns 30) "span"]
-          [(ss/listbox :id :xlist :model ["aaa", "bbb", "ccc"]) "span"]
-          [(ss/text :id :xtext
-                    :text "Hi Mom!!" :editable? true :columns 30 :multi-line? :true :rows 2) "wrap"]
-          [(ss/text :id :logtext
-                    :text "Logging.." :editable? false :columns 30 :multi-line? true :rows 10)]
-          ]))
+  (let [lp (ssm/mig-panel
+            :id :panel
+            :items[
+                   ["Propeller"        "split, span, gaptop 10"]
+                   [(ss/toggle :id :pencil  :class :tool :text "Pencil" )]
+                   [(ss/toggle :id :line    :class :tool :text "Line")]
+                   [(ss/combobox :id :stroke :class :style :model [1 2 3 5 8 13 21])"wrap"]
+                   [(ss/text :id :text-in
+                             :text "input" :editable? true :columns 30) "span"]
+                   [(ss/listbox :id :xlist :model ["aaa", "bbb", "ccc"]) "span"]
+                   [(ss/text :id :xtext
+                             :text "Hi Mom!!" :editable? true :columns 30 :multi-line? :true :rows 2) "wrap"]
+                   [(ss/text :id :text-log
+                             :text "Logging.." :editable? false :columns 30 :multi-line? true :rows 5)]
+                   ])
+        rp (ssm/mig-panel
+            :id :panel
+            :items[
+                   [(ss/text :id :text-log
+                             :text "Your text here..." :editable? true :columns 50 :multi-line? true :rows 30)]
+                   ])]
+    (ss/left-right-split lp rp)))
+    
+
   
 (defn make-frame []
   (ss/frame
