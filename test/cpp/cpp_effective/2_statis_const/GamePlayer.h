0095728b6b582386d44b6a74cfce653d654f3566
kowaraj <kowaraj4stuff@gmail.com>
2016-09-02 23:45:29 +0200

    fixed

--- test/cpp/cpp_effective/2_statis_const/GamePlayer.h
+++ test/cpp/cpp_effective/2_statis_const/GamePlayer.h
@@ -2,8 +2,15 @@ class GamePlayer {
 
 private:
 	static const int NumTurns = 5; // declaration !
-	static const int NumTurns_oldCompiler; // old compiler doesn't allow definition here
+
+	// 1. Error:
+	// static const int NumTurns_oldCompiler; // old compiler doesn't allow definition here
+	// int scores[NumTurns_oldCompiler]; // compilation error: not defined
+
+	// 2. Solution:
+	enum  { NumTurns_oldCompiler = 5 } ; // old compiler doesn't allow definition here
 	int scores[NumTurns_oldCompiler]; // compilation error: not defined
+	
 	
 public:
 	void show();
