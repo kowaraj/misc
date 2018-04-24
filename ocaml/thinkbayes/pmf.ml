e8e8aa358791ead166f492f964cecba3239c9532
Andrey Pashnin <kapashnin@ams-mbpash.dyndns.cern.ch>
2018-04-24 13:35:28 +0200

    Add Normalize() to Pmf

--- ocaml/thinkbayes/pmf.ml
+++ ocaml/thinkbayes/pmf.ml
@@ -1,19 +1,34 @@
 module Pmf = 
   struct
-    type t = (string, int) Hashtbl.t;;
+    type t = (string, float) Hashtbl.t;;
     let empty () = Hashtbl.create 10;;
     let ht_string () = Hashtbl.create 10;;
               
     let f ht d x = 
       Hashtbl.add ht d x;;
 
-    let add ht d = 
-      let cnt = (Hashtbl.find_opt ht d) in 
-      Hashtbl.add ht d (cnt + 1);;
+    let get_count ht d =
+      try 
+        (Hashtbl.find ht d);
+      with e ->
+        0.0
+    ;;
+
+    let add ht k = 
+      Format.printf "Add %s\n" k;
+      Hashtbl.replace ht k ((get_count ht k) +. 1.0);
+    ;;
+
+    let normalize ht =
+      Hashtbl.fold (fun k d acc -> acc +. d) ht 0.0
+    ;;      
+              
+                
+      
 
     let print_string ht = 
-      Hashtbl.iter (fun k v -> Format.printf "%s\n" k) ht;;
-    let print_int ht = 
+      Hashtbl.iter (fun k v -> Format.printf "%s => %f\n" k v) ht;;
+    let print_float ht = 
       Hashtbl.iter (fun k v -> Format.printf "%d => %f\n" k v) ht;;
     (* Hashtbl.fold (fun k v acc -> (k, v) :: acc) ht [];; *)
     (* (fun h -> Hashtbl.fold (fun k v acc -> (k, v) :: acc) h []) ht;;(\*@@ Pmf.empty ();;*\) *)
