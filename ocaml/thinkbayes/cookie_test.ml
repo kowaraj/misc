17d00e13dbc9abe446bdb8149ed516770be23a5f
Andrey Pashnin <kapashnin@ams-mbpash.dyndns.cern.ch>
2018-05-01 19:05:00 +0200

    ...

--- ocaml/thinkbayes/cookie_test.ml
+++ ocaml/thinkbayes/cookie_test.ml
@@ -2,13 +2,13 @@ open Cookie
 
 let hypos = ["Bowl1"; "Bowl2"] (*; "Bowl3"];;*)
 let pmf  = Cookie.cookie hypos;; (* Fill and Normalize *)
+
 Pmf.print_string pmf;;
 
+let mixes = Cookie.mixes in 
+    Cookie.update pmf mixes "vanilla"
+;;
 
-Pmf.mult ht "Bowl1" 0.75;;
-Pmf.mult ht "Bowl2" 0.5;;
-
-Pmf.normalize ht;;
 
 Format.printf "P(Bowl 1) = %f\n" (Pmf.prob ht "Bowl1");;
 
