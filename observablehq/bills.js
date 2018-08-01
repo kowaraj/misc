691c9f82fc63c577e521b0aa7acd85fd6e22a8e2
Andrey Pashnin <kapashnin@ams-mbpash.dyndns.cern.ch>
2018-08-01 11:26:27 +0200

    Add some descriptions. Beutify.

--- observablehq/bills.js
+++ observablehq/bills.js
@@ -1,22 +1,44 @@
 // URL: https://beta.observablehq.com/@kowaraj/bills
-// Title: Bookkeeping of a shared appartment
+// Title: Bookkeeping of a shared appartment\n\n *Version 1.2 @20180801. Comments, suggestions are very welcome!*
 // Author: kowaraj (@kowaraj)
-// Version: 1271
+// Version: 1572
 // Runtime version: 1
 
 const m0 = {
-  id: "ef29a3386956098e@1271",
+  id: "ef29a3386956098e@1572",
   variables: [
     {
       inputs: ["md"],
       value: (function(md){return(
-md`# Bookkeeping of a shared appartment`
+md`# Bookkeeping of a shared appartment\n\n *Version 1.2 @20180801. Comments, suggestions are very welcome!*`
+)})
+    },
+    {
+      inputs: ["md","accounts","roundDebts","debts"],
+      value: (function(md,accounts,roundDebts,debts){return(
+md`## Summary\n\n
+${
+Object.keys(accounts).map(a => {
+  
+return "\n\n- #### " + accounts[a].name + " owes to \n" 
+  + Object.keys(roundDebts(debts[a].debts)).map(
+    k => {
+      const x = roundDebts(debts[a].debts)[k]
+      if (x > 1)
+        return ""+k+" : "+x
+      else
+        return ""+k+" : --" 
+    })
+    .map(x => " - *"+x+"*").join("\n\n")
+})                   
+}
+`
 )})
     },
     {
       inputs: ["md"],
       value: (function(md){return(
-md`Pick the dates to account. Default is from the day Nicola arrived up to today.`
+md`### Dates \n\n *We start from 2017.06.01. The day that the first transactions (calculated by Irene) accounted for (and it brakes if you try to start from the first day of Nicola's arrival).*`
 )})
     },
     {
@@ -29,13 +51,7 @@ md`Pick the dates to account. Default is from the day Nicola arrived up to today
     {
       inputs: ["md"],
       value: (function(md){return(
-md`# Accounts`
-)})
-    },
-    {
-      inputs: ["md"],
-      value: (function(md){return(
-md`Accounts show the balance and the debts`
+md`\n---\n\n*The rest just code...*\n\n---\n\n# Accounts\n\n*Accounts show the balance and the debts*`
 )})
     },
     {
@@ -77,13 +93,7 @@ Object.assign({balance: debts[1].balance.toFixed(2)},  roundDebts(debts[1].debts
     {
       inputs: ["md"],
       value: (function(md){return(
-md`# Bills`
-)})
-    },
-    {
-      inputs: ["md"],
-      value: (function(md){return(
-md`Selected bills (see the chosen dates above)`
+md`# Bills\n\n*Selected bills (see the chosen dates above)*`
 )})
     },
     {
@@ -100,13 +110,7 @@ bills.filter(x => {
     {
       inputs: ["md"],
       value: (function(md){return(
-md`# Transactions`
-)})
-    },
-    {
-      inputs: ["md"],
-      value: (function(md){return(
-md`Who paid whom and what for. For the first three the date is 'fake', Nov 11 2017`
+md`# Transactions\n\n*Who paid whom, how much and what for... ;)*`
 )})
     },
     {
@@ -117,12 +121,6 @@ md`Who paid whom and what for. For the first three the date is 'fake', Nov 11 20
    {date: "2017-11-11", value: 49.47, description: "for edf", from: "Nicola", to: "Irene"},
    {date: "2017-11-11", value: 58.11, description: "for edf", from: "Nicola", to: "Andrey"}
   ]
-)})
-    },
-    {
-      inputs: ["md"],
-      value: (function(md){return(
-md`Let it be the day when we were even, Nov 11 2017. This would be the list of Transactions.`
 )})
     },
     {
@@ -143,7 +141,7 @@ md`List of all the bills paid since 01.11.2017 (Andrey arrived)`
 [
        {date: "2017-05-28", value: 247.65, people: 2, type: "edf", paidby: "Andrey", note: "-May 2017"},
        {date: "2017-07-30", value: 64.6,   people: 2, type: "internet", paidby: "Irene", note: "Jun-Jul 2017"},
-       {date: "2017-08-16", value: 172.24, people: 2, type: "edf", paidby: "Andrey", note: "May-Aug 2017"},
+       {date: "2017-07-30", value: 172.24, people: 2, type: "edf", paidby: "Andrey", note: "May-Aug 2017"},
        {date: "2017-09-30", value: 150.00,  people: 3, type: "internet", paidby: "Irene", note: "Aug-Oct 2017"},
        {date: "2017-09-26", value: 174.33, people: 3, type: "edf", paidby: "Andrey", note: "Aug-Sep 2017"},
        {date: "2017-11-26", value: 330.55, people: 3, type: "edf", paidby: "Andrey", note: "Sep-Nov 2017"},
@@ -160,7 +158,7 @@ md`List of all the bills paid since 01.11.2017 (Andrey arrived)`
   //{date: "2000-01-01", people: [], 
   //                     note: "Fake First date"},
   {date: "2016-11-01", people: [{room: 2, name:"Irene"}, {room: 3, name:"Andrey"}] },
-  {date: "2017-09-01", people: [{room: 1, name:"Nicola"}, {room: 2, name:"Irene"}, {room: 3, name:"Andrey"} ], 
+  {date: "2017-08-01", people: [{room: 1, name:"Nicola"}, {room: 2, name:"Irene"}, {room: 3, name:"Andrey"} ], 
                        note: "Nicola arrived"}//,
   //{date: "2999-01-01", people: [],
   //                     note: "Fake Last date"}
@@ -176,6 +174,8 @@ md`List of all the bills paid since 01.11.2017 (Andrey arrived)`
   const accs0 = [];  
   people.forEach(x => accs0.push({name:x, balance:0, debts: getPeopleButOneAsObject(x), owes:[]}));
   const temp = accs0.map(a => ({...a}));
+  //temp.forEach(a => console.log("temp accounts: "+ a.name + ", " + a.balance + ", D= ", a))
+
   const ret = selectedbills.reduce( (acc,cur) => processBill(acc, cur), temp)
   ret.forEach(x=>Object.keys(x.debts).forEach(k=>console.log("debts[k] = ", x.debts[k])))
   console.log("end")
@@ -185,11 +185,11 @@ md`List of all the bills paid since 01.11.2017 (Andrey arrived)`
     },
     {
       name: "debts",
-      inputs: ["people","accounts","transactions"],
-      value: (function(people,accounts,transactions)
+      inputs: ["accounts","people","transactions"],
+      value: (function(accounts,people,transactions)
 {
-  console.log("recalc debts...")
-
+  console.log("\nrecalc debts...")
+  accounts.forEach(a => console.log("account: "+ a.name + ", " + a.balance + " = ", a.debts))
   const innerloop = (name) => {
   return Array.from(people).filter(p => p != name).reduce (
                      (acc,cur) => {return Object.assign( { [cur] : 0 }, acc )},
@@ -204,6 +204,7 @@ md`List of all the bills paid since 01.11.2017 (Andrey arrived)`
   accs1.forEach(a => {    a.owes.forEach(o => { a.debts[o.whom] += o.amount; })     })
 
   // substructing debts of the opposite
+  console.log("opposite debts substruction...")
   const accs2 = accounts.map(x => ({...x}))
   accs2.forEach(a2 => {    
     Object.keys(a2.debts).forEach(dk => { 
@@ -214,6 +215,7 @@ md`List of all the bills paid since 01.11.2017 (Andrey arrived)`
   })
   
   //process Transactions for 'balance'
+  console.log("Transactions...")
   transactions.forEach(tr => {
     console.log("tr = ", tr)
     
@@ -251,16 +253,18 @@ md`List of all the bills paid since 01.11.2017 (Andrey arrived)`
     },
     {
       name: "processBill",
-      inputs: ["getPeopleFromBill"],
-      value: (function(getPeopleFromBill){return(
+      inputs: ["getPeopleFromBill","inhabitants"],
+      value: (function(getPeopleFromBill,inhabitants){return(
 (accounts_, bill) => {
-  //console.log("bills = ", bill)
-  const people = getPeopleFromBill(bill)
+  console.log("\nbills = ", bill)
+  const people = getPeopleFromBill(bill, inhabitants)
   accounts_.forEach(a => {
-    //console.log("a = ", a)
+    console.log("a = ", a)
     if (a.name == bill.paidby) {
+      console.log("payer")
       a.balance -= bill.value;
     } else if (people.includes(a.name)) {
+      console.log("debitor")
       a.owes.push( {whom: bill.paidby, amount: bill.value / people.length, for: bill.type} );
       //a.debts
     } else {
@@ -282,11 +286,12 @@ inhabitants.reduce( (acc, cur) => {
     },
     {
       name: "getPeopleFromBill",
-      inputs: ["inhabitants"],
-      value: (function(inhabitants){return(
-(bill) => { 
-  const idx = inhabitants.sort((x,y)=>(x.date < y.date)).findIndex(i => i.date <= bill.date)
-  return inhabitants[idx].people.map(p=>p.name);
+      value: (function(){return(
+(bill, inh_) => { 
+  const idx = inh_.sort((x,y)=>(x.date < y.date)).findIndex(i => i.date <= bill.date)
+  const pp = inh_[idx].people.map(p=>p.name)
+  //console.log("people from bill = ", pp)
+  return pp;
 }
 )})
     },
@@ -332,8 +337,14 @@ md`Fix *getPeopleFromBill()*`
 )})
     },
     {
-      inputs: ["getPeopleFromBill"],
-      value: (function(getPeopleFromBill){return(
+      inputs: ["md"],
+      value: (function(md){return(
+md`# Some unit tests...`
+)})
+    },
+    {
+      inputs: ["getPeopleFromBill","inhabitants"],
+      value: (function(getPeopleFromBill,inhabitants){return(
 getPeopleFromBill({
   date: "2017-09-02",
   value: 247.65,
@@ -341,7 +352,7 @@ getPeopleFromBill({
   type: "edf",
   paidby: "Andrey",
   note: "-May 2017",
-}).length == 3
+}, inhabitants).length == 3
 )})
     },
     {
@@ -353,12 +364,40 @@ getPeopleFromBill({
   return debts.map(d => (Object.assign(  { name: d.name}, roundDebts(d.debts)) )) 
 }
 )
+    },
+    {
+      name: "test_01_inhabitants",
+      value: (function(){return(
+[ 
+  //{date: "2000-01-01", people: [], 
+  //                     note: "Fake First date"},
+  {date: "2016-11-01", people: [{room: 2, name:"Irene"}, {room: 3, name:"Andrey"}] },
+  {date: "2017-08-01", people: [{room: 1, name:"Nicola"}, {room: 2, name:"Irene"}, {room: 3, name:"Andrey"} ], 
+                       note: "Nicola arrived"}//,
+  //{date: "2999-01-01", people: [],
+  //                     note: "Fake Last date"}
+  ]
+)})
+    },
+    {
+      name: "test_01_bills",
+      value: (function(){return(
+[   {date: "2017-07-30", value: 172.24, people: 2, type: "edf", paidby: "Andrey", note: "May-Aug 2017"}
+                 ]
+)})
+    },
+    {
+      name: "RESULT_OF__test_01",
+      inputs: ["test_01_bills","getPeopleFromBill","test_01_inhabitants"],
+      value: (function(test_01_bills,getPeopleFromBill,test_01_inhabitants){return(
+test_01_bills.map(b => getPeopleFromBill(b, test_01_inhabitants))[0].length == 2
+)})
     }
   ]
 };
 
 const notebook = {
-  id: "ef29a3386956098e@1271",
+  id: "ef29a3386956098e@1572",
   modules: [m0]
 };
 
